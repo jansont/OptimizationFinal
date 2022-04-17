69f52ee6276979af200e4e4b62fc5b88b6dc10ee
Theodore Janson <jansontheo@gmail.com>
2022-04-16 20:20:05 -0400

    vis

--- algorithms.py
+++ algorithms.py
@@ -8,10 +8,12 @@ def steepest_descent(x0,
                      cost_function,
                      gradient_function,
                      step_size = 'armijo',
-                     threshold = 1e-8, 
+                     threshold = 1e-4, 
                      log = False, 
                      h = 1e-8, 
-                     max_iter = 1e12, 
+                     max_iter = 1e6, 
+                     gamma = 1.5, 
+                     r = 0.8, 
                      fd_method = 'central', 
                      track_history = False):
     '''
@@ -30,14 +32,18 @@ def steepest_descent(x0,
             Default: 'armijo
         threshold :: float
             Threshold at which to stop minimization. Values 
-            should be close to 0. Default: 1e-8
+            should be close to 0. Default: 1e-4
         log :: bool
             True to log optimization progress. Default: False
         h :: float
             Parameter for finite difference estimation. 
             Default 1e-8
         max_iter :: int
-            Maximum optimization iterations. Default: 1e12
+            Maximum optimization iterations. Default: 1e6
+        gamma :: float
+            Gamma parameter for armijo. Default is 1.5. 
+        r :: float
+            r parameter for armijo. Default is 0.8. 
         fd_method :: string
             Method for finite difference estimation. 
             Options: 'central', 'forward'
@@ -64,24 +70,26 @@ def steepest_descent(x0,
     i = 0
     x = x0
     gradient = gradient_function(x)
+    minimum = cost_function(x)
 
     #iterate until near zero gradient or max iterations reached
     while norm(gradient) >= threshold and i <= max_iter: 
 
         #update gradient
         gradient = gradient_function(x)
+        search_dir = -1*gradient
 
-        #determine step size
-        if step_size == 'armijo':
-            step_size = armijo(x, cost_function, gradient_function, gradient)
+    #determine step size
+        if step_size == 'armijo':                      
+            w = armijo(x, cost_function, gradient, search_dir=search_dir.flatten(), gamma = gamma, r = r, log=False)
         elif isinstance(step_size, (int, float)):
-            continue
+            w = step_size
         else: 
-            raise ValueError('step size should be float, int or "armijo"')
+            raise TypeError('step size should be float, int or "armijo"') 
         
         # move to a new x by moving from the original x in the negative
         # direction of the gradient according to a given step size
-        x = x - step_size*gradient
+        x = x + w*search_dir.flatten()
         minimum = cost_function(x)
 
         #result tracking
@@ -107,7 +115,9 @@ def conjugate_gradient(x0,
                      threshold = 1e-8, 
                      log = False, 
                      h = 1e-8, 
-                     max_iter = 1e12, 
+                     max_iter = 1e6, 
+                     gamma = 1.5, 
+                     r = 0.8, 
                      fd_method = 'central', 
                      track_history = False):
     '''
@@ -132,8 +142,12 @@ def conjugate_gradient(x0,
         h :: float
             Parameter for finite difference estimation. 
             Default 1e-8
+        gamma :: float
+            Gamma parameter for armijo. Default is 1.5. 
+        r :: float
+            r parameter for armijo. Default is 0.8. 
         max_iter :: int
-            Maximum optimization iterations. Default: 1e12
+            Maximum optimization iterations. Default: 1e6
         fd_method :: string
             Method for finite difference estimation. 
             Options: 'central', 'forward'
@@ -158,29 +172,143 @@ def conjugate_gradient(x0,
     i = 0
     prev_gradient = gradient_function(x0)
     search_direction = prev_gradient * -1
-
+    minimum = cost_function(x0)
     while norm(prev_gradient) >= threshold and i <= max_iter: 
-        
+
         #determine step size
         if step_size == 'armijo':
-            step_size = armijo(x, cost_function, gradient_function, prev_gradient)
+            w = armijo(x0,
+                    cost_function,
+                    prev_gradient,
+                    search_dir=search_direction.flatten(),
+                    gamma = gamma,
+                    r = r,
+                    log=False)
+
         elif isinstance(step_size, (int, float)):
-            continue
+            w = step_size
         else: 
-            raise ValueError('step size should be float, int or "armijo"')
+            raise TypeError('step size should be float, int or "armijo"')
 
         #conjugate_gradient_algorithm
-        x1 = x0 + step_size * search_direction
+        x1 = x0 + w * search_direction
         next_gradient = gradient_function(x1)
         beta = (next_gradient - prev_gradient) @  next_gradient
         beta /= prev_gradient @ prev_gradient.transpose()
         search_direction = -1*next_gradient + beta * search_direction
         prev_gradient = next_gradient
         x0 = x1
-        minimum = cost_function(x0)
+        minimum = cost_function(x1)
 
         #track results
         if log and i%1e4 == 0: 
+            print(f'x = {x1}, V(x) = {minimum:.5f}')
+        if track_history: 
+            x_history.append(x1), V_history.append(minimum)
+
+    if track_history:
+        return x1, minimum, x_history, V_history
+    else: 
+        return x1, minimum
+
+def secant(x0,
+            cost_function,
+            gradient_function,
+            H = None,
+            step_size = 'armijo',
+            threshold = 1e-8, 
+            log = False, 
+            h = 1e-8, 
+            max_iter = 1e6, 
+            gamma = 1.5, 
+            r = 0.8,
+            fd_method = 'central', 
+            track_history = False):
+    '''
+    Performs minimization using secant algorithm with Davidson-Fletcher-Powell.  
+    Args: 
+        x0 :: np.array
+            Initial point of minization. Shape (n,)
+        cost_function :: Python function
+            Cost function to minimize. Rn -> R. 
+        gradient_function :: Python function, or None
+            Gradient of cost_function. Rn -> Rn
+            If None, finite difference estimation of gradient is used. 
+        H :: np.array (shape: len(x0) x len(x0))
+            Estimate for C inverse. Default is None.
+            if None, H = eye(len(x0)) is used. 
+        step_size :: float or String
+            Step size to use during gradient descent. 
+            If 'armijo', Armijo step size selection is used. 
+            Default: 'armijo
+        threshold :: float
+            Threshold at which to stop minimization. Values 
+            should be close to 0. Default: 1e-8
+        log :: bool
+            True to log optimization progress. Default: False
+        h :: float
+            Parameter for finite difference estimation. 
+            Default 1e-8
+        gamma :: float
+            Gamma parameter for armijo. Default is 1.5. 
+        r :: float
+            r parameter for armijo. Default is 0.8. 
+        max_iter :: int
+            Maximum optimization iterations. Default: 1e6
+        fd_method :: string
+            Method for finite difference estimation. 
+            Options: 'central', 'forward'
+        track_history :: bool
+            True to track points visited and corresponding cost. 
+    Returns: 
+        x :: np.array
+            Point at which minimization is reached. Shape (n,)
+        minimum :: float
+            Value of cost function at optimizer. 
+        x_history :: list
+            List of points visisted. (if track_history = True)
+        V_history :: list
+            List of costs visisted. (if track_history = True)
+    '''
+    if H == None:
+        H = np.eye(len(x0))
+    if H.shape[0] != H.shape[1] != len(x0):
+        raise ValueError('H should be square numpy array with n = len(x0).')
+
+    #if no gradient function available, use finite difference appx
+    if gradient_function == None: 
+        fd = Finite_Difference(cost_function, fd_method, h)
+        gradient_function = fd.estimate_gradient
+
+    j=0
+    x_history, V_history = [],[]
+    minimum = cost_function(x0)
+    while True:
+        gradient_x0 = gradient_function(x0)
+        s = -np.matmul(H,gradient_x0.reshape(-1,1))
+        
+        #determine step size
+        if step_size == 'armijo':                      
+            w = armijo(x0, cost_function, gradient_x0, search_dir=s.flatten(), gamma = gamma, r = r, log=False)
+        elif isinstance(step_size, (int, float)):
+            w = step_size
+        else: 
+            raise TypeError('step size should be float, int or "armijo"') 
+        x1 = x0 + w*s.flatten()
+        gradient_x1 = gradient_function(x1)
+        if norm(gradient_x1) < threshold or j > max_iter:
+            break
+        dx = (x1-x0).reshape(-1,1)
+        dg = (gradient_x1-gradient_x0).reshape(-1,1)
+        H = H + np.matmul(dx,dx.reshape(1,-1))/np.dot(dx.reshape(1,-1),dg) - np.matmul(np.matmul(H,dg),(np.matmul(H,dg)).reshape(1,-1))/np.dot(dg.reshape(1,-1),np.matmul(H,dg))
+        j += 1
+        x0 = x1
+        minimum  = cost_function(x0)
+
+        x_history.append(x0), V_history.append(minimum)
+
+        #track results
+        if log and j%1e4 == 0: 
             print(f'x = {x0}, V(x) = {minimum:.5f}')
         if track_history: 
             x_history.append(x0), V_history.append(minimum)
@@ -189,8 +317,6 @@ def conjugate_gradient(x0,
         return x0, minimum, x_history, V_history
     else: 
         return x0, minimum
-
-
 
 
 class Finite_Difference:
@@ -249,115 +375,9 @@ class Finite_Difference:
         elif self.method == 'forward': 
             return self.forward_difference(x)
         else: 
-            raise ValueError("Method must be 'central' or 'forward'")
+            raise TypeError("Method must be 'central' or 'forward'")
 
 
-
-def secant(x0,
-            cost_function,
-            gradient_function,
-            H = None,
-            step_size = 'armijo',
-            threshold = 1e-8, 
-            log = False, 
-            h = 1e-8, 
-            max_iter = 1e12, 
-            fd_method = 'central', 
-            track_history = False):
-    '''
-    Performs minimization using secant algorithm with Davidson-Fletcher-Powell.  
-    Args: 
-        x0 :: np.array
-            Initial point of minization. Shape (n,)
-        cost_function :: Python function
-            Cost function to minimize. Rn -> R. 
-        gradient_function :: Python function, or None
-            Gradient of cost_function. Rn -> Rn
-            If None, finite difference estimation of gradient is used. 
-        H :: np.array (shape: len(x0) x len(x0))
-            Estimate for C inverse. Default is None.
-            if None, H = eye(len(x0)) is used. 
-        step_size :: float or String
-            Step size to use during gradient descent. 
-            If 'armijo', Armijo step size selection is used. 
-            Default: 'armijo
-        threshold :: float
-            Threshold at which to stop minimization. Values 
-            should be close to 0. Default: 1e-8
-        log :: bool
-            True to log optimization progress. Default: False
-        h :: float
-            Parameter for finite difference estimation. 
-            Default 1e-8
-        max_iter :: int
-            Maximum optimization iterations. Default: 1e12
-        fd_method :: string
-            Method for finite difference estimation. 
-            Options: 'central', 'forward'
-        track_history :: bool
-            True to track points visited and corresponding cost. 
-    Returns: 
-        x :: np.array
-            Point at which minimization is reached. Shape (n,)
-        minimum :: float
-            Value of cost function at optimizer. 
-        x_history :: list
-            List of points visisted. (if track_history = True)
-        V_history :: list
-            List of costs visisted. (if track_history = True)
-    '''
-    if H == None:
-        H = np.eye(len(x0))
-    if H.shape[0] != H.shape[1] != len(x0):
-        raise ValueError('H should be square numpy array with n = len(x0).')
-
-    #if no gradient function available, use finite difference appx
-    if gradient_function == None: 
-        fd = Finite_Difference(cost_function, fd_method, h)
-        gradient_function = fd.estimate_gradient
-    
-    j=0
-    x_history, V_history = [], []
-
-    while True:
-        gradient_x0 = gradient_function(x0)
-        s = -np.matmul(H,gradient_x0.reshape(-1,1))
-
-        #determine step size
-        if step_size == 'armijo':
-            w = armijo(x0, cost_function, gradient_x0, search_dir=s.flatten(), gamma=1.5, r=0.8, log=False)
-        elif isinstance(step_size, (int, float)):
-            w = step_size
-        else: 
-            raise ValueError('step size should be float, int or "armijo"')
-        
-        
-        x1 = x0 + w*s.flatten()
-        gradient_x1 = gradient_function(x1)
-
-        if norm(gradient_x1) < threshold or j > max_iter:
-            break
-
-        dx = (x1-x0).reshape(-1,1)
-        dg = (gradient_x1-gradient_x0).reshape(-1,1)
-        H = H + np.matmul(dx,dx.reshape(1,-1))/np.dot(dx.reshape(1,-1),dg) 
-        H -= np.matmul(np.matmul(H,dg),(np.matmul(H,dg)).reshape(1,-1))/np.dot(dg.reshape(1,-1),np.matmul(H,dg))
-        
-        j += 1
-        x0 = x1
-        minimum = cost_function(x0)
-        x_history.append(x0), V_history.append(minimum)
-
-        #track results
-        if log and j%1e4 == 0: 
-            print(f'x = {x0}, V(x) = {minimum:.5f}')
-        if track_history: 
-            x_history.append(x0), V_history.append(minimum)
-
-    if track_history:
-        return x0, minimum, x_history, V_history
-    else: 
-        return x0, minimum
 
 
 
@@ -391,28 +411,70 @@ def armijo(x,
             Optimal step size at x in direction search_dir. 
     '''
     
-    def v_bar(cost_x,grad_x_s,w):
+    def v_bar(w):
         return cost_x + 0.5*w*grad_x_s
 
-    w = 0.1
+    w = 1
     cost_x = cost_function(x)
-    grad_x_s = gradient @ search_dir
+    grad_x_s = np.dot(gradient,search_dir)
     # initialize p
     p = 0
     # propogate forward
-    while cost_function(x + (gamma**p)*search_dir) < v_bar(cost_x, grad_x_s, (gamma**p)): 
+    w = gamma**p
+    while cost_function(x + w*search_dir) < v_bar(w): 
         w = gamma**p
         # increment p
         p += 1
     # initialize q
     q = 0
     # propogate backwards
-    while cost_function(x + (r**q * gamma**p)*search_dir) > v_bar(cost_x, grad_x_s, r**q * gamma**p): 
-        # consider step size w
-        w = r**q * gamma**p
+    w = r**q * gamma**p
+    while cost_function(x + w*search_dir) > v_bar(w): 
         # increment q
         q += 1
+        # consider step size w
+        w = r**q * gamma**p
+
     # return step size
     if log:
         print(f'p={p}, q={q}, w={w}')
     return w
+
+
+# def armijo(x, cost_function, gradient, s, gamma=1.5, mu=0.8):
+#     """Armijo algorithm for computing step size
+
+#     Parameters
+#     ----------
+#     gamma : float
+#         Parameter for increasing step size
+#     mu : float
+#         Parameter for decreasing step size
+
+#     Returns
+#     -------
+#     float
+#         Step size
+#     """
+
+#     w = 1  # Default step size
+
+#     k_g = 0  # Power of gamma
+#     k_m = 0  # Power of mu
+
+#     # Precompute cost and gradient to save time
+#     vx = cost_function(x)
+#     gx_s = gradient @ s
+
+#     def v_bar(w):
+#         return vx + 0.5 * w * gx_s
+
+#     while p.cost(x + gamma**k_g * s) < v_bar(gamma**k_g):
+#         k_g += 1
+#         w = gamma**k_g
+
+#     while p.cost(x + mu**k_m * gamma**k_g * s) > v_bar(mu**k_m * gamma**k_g):
+#         k_m += 1
+#         w = mu**k_m * gamma**k_g
+
+#     return w
--- cost_functions.py
+++ cost_functions.py
@@ -4,25 +4,28 @@ from numpy.linalg import norm, eig
 #-_____________COST FUNCTION A___________________
 #COST FUNCTION A
 def V_a(x):
-    a = np.array([5])
+    if x.shape[0] != 6:
+        raise ValueError('X must have dimension 6.')
     b = np.array([1, 4, 5, 4, 2, 1])
     C = [[9, 1, 7, 5, 4, 7], 
         [1, 11, 4, 2, 7, 5], 
         [7, 4, 13, 5, 0, 7], 
         [5, 2, 5, 17, 1, 9], 
         [4, 7, 0, 1, 21, 15], 
-        [7, 5, 7, 9, 5, 27]]
+        [7, 5, 7, 9, 15, 27]]
     C = np.array(C)
     return 5 + b@x + x @ (C @ x)
 
 def gradV_a(x):
+    if x.shape[0] != 6:
+        raise ValueError('X must have dimension 6.')
     b = np.array([1, 4, 5, 4, 2, 1])
     C = [[9, 1, 7, 5, 4, 7], 
         [1, 11, 4, 2, 7, 5], 
         [7, 4, 13, 5, 0, 7], 
         [5, 2, 5, 17, 1, 9], 
         [4, 7, 0, 1, 21, 15], 
-        [7, 5, 7, 9, 5, 27]]
+        [7, 5, 7, 9, 15, 27]]
     C = np.array(C)
     return b + 2 * C @ x
 #_________________________________________________
@@ -30,13 +33,17 @@ def gradV_a(x):
 
 #-_____________COST FUNCTION B___________________
 def V_b(x):
-    x1, x2 = x
+    if x.shape[0] != 2:
+        raise ValueError('X must have dimension 2.')
+    x1, x2 = x[0], x[1]
     num = ((x1**2 + 1)*(2*x2**2 + 1))**0.5
     den = x1**2 + x2**2 + 0.5
-    return -num / den
+    return - num / den
 
 def gradV_b(x):
-    x1, x2 = x
+    if x.shape[0] != 2:
+        raise ValueError('X must have dimension 2.')
+    x1, x2 = x[0],x[1]
 
     num = (-x1**3 + x1*x2**2 - 1.5*x1)*(2*x2**2+1)**0.5
     den = (x1**2 + x2**2 + 0.5)**2 * (x1**2 + 1)**0.5
@@ -46,5 +53,35 @@ def gradV_b(x):
     den = (x1**2 + x2**2 + 0.5)**2 * (2*x2**2 + 1)**0.5
     dx2 = -num / den
 
-    return np.array([x1,x2])
+    return np.array([dx1,dx2])
 #_________________________________________________
+
+
+#-_____________COST FUNCTION C___________________
+def V_c(x):
+    if x.shape[0] != 2:
+        raise ValueError('X must have dimension 2.')
+    x1, x2 = x[0], x[1]
+    a = 1
+    b = np.array([1,2])
+    C = np.array([[12, 3],
+                  [3, 10]])
+    d = 10*np.log(1 + x1**4)*np.sin(100*x1)+10*np.log(1 + x2**4)*np.cos(100*x2)   
+    V = a + b@x + 0.5*x @ (C @ x) + d
+    return  V
+
+def gradV_c(x):
+    if x.shape[0] != 2:
+        raise ValueError('X must have dimension 2.')
+    x1, x2 = x[0], x[1]
+    a = 1
+    b = np.array([1,2])
+    C = np.array([[12, 3],
+                  [3, 10]])  
+    grad =  b + C @ x
+    grad_x1, grad_x2 = grad[0], grad[1]
+    grad_x1 += 10 * ( (4*x1**3 * np.sin(100*x1) / (1+x1**4)) + 100*np.sin(100*x1)*np.log(1+x1**4) )
+    grad_x2 += 10 * ( (4*x1**3 * np.sin(100*x1) / (1+x1**4)) - 100*np.cos(100*x1)*np.log(1+x1**4) )
+    return np.array([grad_x1, grad_x2])
+
+
